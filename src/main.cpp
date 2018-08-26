e7f3f5dfa3250988563636459991d66846497c96
Peter <pommengt@gmail.com>
2018-08-26 23:22:34 +0200

    Sunday tinkering

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ Settings:
       Time - need more ledarrays to display properly
 
 todo:
-Fireware 1.0 done.
+Firmware 1.0 done.
 
 
 */
@@ -22,7 +22,7 @@ Fireware 1.0 done.
 #include <TimerOne.h>
 #include <Wire.h>
 #include "RTClib.h"
-
+#include <EEPROM.h>
 #include <UltraDistSensor.h>
 
 //**************************
@@ -30,15 +30,17 @@ Fireware 1.0 done.
 #define DEBUG
 
 //**************************
+void setBrightness();
 void timerIsr();
 void btnstate();
 void setCompileTime();
 void updateDisplay();
 void showTime();
 void pulsingHearts();
-
+void Scollingtext();
 int16_t oldEncPos, encPos;
 uint8_t buttonState;
+uint8_t daylightAddr = 0;
 bool newNumer = 0;
 bool done = 0;
 int numPullups = 0;
@@ -53,20 +55,24 @@ unsigned long hasGoneDownTime = 0;
 bool hasGoneDown = 0;
 uint16_t rangeUpdateTimer = 0;
 uint16_t clockUpdateTimer = 0;
-#define pinA A2
-#define pinB A1
+int daylight = 8;
+#define pinA A1
+#define pinB A2
 #define pinSw A0 //switch on enc
 #define STEPS 4
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 unsigned long lastMoved = 0;
-unsigned long MOVE_INTERVAL = 40; // mS
+uint8_t MOVE_INTERVAL = 40; // mS
 int messageOffset;
+int scollingMessageOffset;
 const byte chips = 2;
 float distanceReading = 0.00;
+bool doneWithScolling = 0;
 
 UltraDistSensor mysensor;
 RTC_DS1307 rtc;
 ClickEncoder encoder(pinA, pinB, pinSw, STEPS);
+DateTime now;
 // 2 chips (display modules), hardware SPI with load on D10
 MAX7219_Dot_Matrix display(chips, 10); // Chips / LOAD
 
@@ -84,25 +90,34 @@ void setup()
       }
       mysensor.attach(6, 7); //Trigger pin , Echo pin
       oldEncPos = 0;
+      daylight = EEPROM.get(daylightAddr, daylight);
+      Serial.print("Brightness : ");
+      Serial.println(daylight);
+      if (daylight > 15 || daylight < 0)
+      {
+            daylight = 8;
+      }
+
+      display.setIntensity(daylight);
+
       setCompileTime();
       //showTime();
 
       //pulsingHearts();
-      /* 
-      while(1){
-            float prints = mysensor.distanceInCm();
-            Serial.println(prints);
-            delay(100); 
-      }
- */
 }
 
 void loop()
 {
       //update things:
 
-      encPos += encoder.getValue();
-      DateTime now = rtc.now();
+      encPos += encoder.getValue(); //check if the encoder has changed
+
+      if (millis() - clockUpdateTimer >= 30000) //read clock every 30 seconds
+      {
+            now = rtc.now();
+            // Serial.print(now.second(), DEC);
+      }
+
       if (millis() - rangeUpdateTimer >= 100)
       {
             distanceReading = mysensor.distanceInCm();
@@ -121,8 +136,8 @@ void loop()
       {
             pullUpTime = millis();
       }
-      else
-            Serial.println(millis() - pullUpTime);
+      //else
+      // Serial.println(millis() - pullUpTime);
 
       if (millis() - pullUpTime > 500 && hasGoneDown == 1)
       {
@@ -197,40 +212,45 @@ void loop()
       }
 
       //Turn off the LED's at 'out of office hours'
-      if (now.hour() < 6 || now.hour() > 18)
+      if (!LEDOff)
       {
+            if (now.hour() < 6 || now.hour() > 18)
+            {
 
 #ifndef DEBUG
 
-            display.end();
+                  display.end();
+                  LEDOff = 1;
 #else
-            display.setIntensity(0);
+                  display.setIntensity(0);
+                  LEDOff = 1;
 
 #endif // DEBUG
+            }
+            else
+            {
+                  display.setIntensity(daylight);
+                  LEDOff = 0;
+            }
       }
-      else
-            display.setIntensity(8);
+
+      btnstate(); //have we pressed the encoder button?
 
 } // end of loop
 
 void updateDisplay()
 {
-      /*       if (newNumer) //starting out to the side if there's a new numer in mid sweep
-      {
-            messageOffset = -chips * 8;
-            newNumer = 0;
-                              done = 0;
-
-      }
- */
       itoa(numPullups, message, 10); //int to string
+
       if (done)
       {
             display.sendString(message);
       }
       else
       {
+            Serial.print("messageOffset: ");
 
+            Serial.println(messageOffset);
             display.sendSmooth(message, messageOffset);
             // next time show one pixel onwards
             if (messageOffset++ >= (int)(strlen(message) * 8))
@@ -251,18 +271,6 @@ void btnstate()
 
       buttonState = encoder.getButton();
 
-      switch (buttonState)
-      {
-      case 3:
-            buttonState = 0;
-            break;
-      case 0:
-            buttonState = 0;
-
-      default:
-            break;
-      }
-
       if (buttonState != 0)
       {
             Serial.print("Button: ");
@@ -276,9 +284,11 @@ void btnstate()
                   break;
 
             case ClickEncoder::Pressed: //2
+
                   break;
 
             case ClickEncoder::Held: //3
+                  setBrightness();
 
                   break;
 
@@ -286,6 +296,7 @@ void btnstate()
                   break;
 
             case ClickEncoder::Clicked: //5
+                  numPullups = 0;
                   break;
 
             case ClickEncoder::DoubleClicked: //6
@@ -301,31 +312,31 @@ void setCompileTime()
       if (!rtc.isrunning())
       {
             Serial.println("RTC is NOT running!");
-            // following line sets the RTC to the date & time this sketch was compiled
-            rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-            // This line sets the RTC with an explicit date & time, for example to set
-            // January 21, 2014 at 3am you would call:
-            // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
       }
+      Serial.print(" Setting RTC to compile time: ");
 
-      Serial.print(F(__DATE__));
-      Serial.print(" : ");
+      // following line sets the RTC to the date & time this sketch was compiled
+      rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+      // This line sets the RTC with an explicit date & time, for example to set
+      // January 21, 2014 at 3am you would call:
+      // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
 
-      Serial.print(F(__TIME__));
-
-      Serial.println();
-      Serial.println();
-      DateTime now = rtc.now();
-
+      now = rtc.now();
       lastDay = now.day();
+      showTime();
 }
 
 void showTime()
 {
-
+      Serial.print("Compile time: ");
+      Serial.print(F(__DATE__));
+      Serial.print(" : ");
+      Serial.print(F(__TIME__));
       Serial.println();
+      Serial.println();
+      Serial.println("Current Time: ");
 
-      DateTime now = rtc.now();
+      now = rtc.now();
       Serial.print(now.year(), DEC);
       Serial.print('/');
       Serial.print(now.month(), DEC);
@@ -341,27 +352,6 @@ void showTime()
       Serial.print(now.second(), DEC);
       Serial.println();
 
-      Serial.print(" since midnight 1/1/1970 = ");
-      Serial.print(now.unixtime());
-      Serial.print("s = ");
-      Serial.print(now.unixtime() / 86400L);
-      Serial.println("d");
-
-      // calculate a date which is 7 days and 30 seconds into the future
-      DateTime future(now + TimeSpan(7, 12, 30, 6));
-
-      Serial.print(" now + 7d + 30s: ");
-      Serial.print(future.year(), DEC);
-      Serial.print('/');
-      Serial.print(future.month(), DEC);
-      Serial.print('/');
-      Serial.print(future.day(), DEC);
-      Serial.print(' ');
-      Serial.print(future.hour(), DEC);
-      Serial.print(':');
-      Serial.print(future.minute(), DEC);
-      Serial.print(':');
-      Serial.print(future.second(), DEC);
       Serial.println();
       if (!rtc.isrunning())
       {
@@ -395,5 +385,47 @@ void pulsingHearts()
                   countDown = 1;
             if (i == 0)
                   countDown = 0;
+      }
+}
+
+void setBrightness()
+{
+      unsigned long lastMovedScolling = 0;
+
+      while (buttonState != 5)
+      {
+            daylight += encoder.getValue();
+            if (daylight < 0)
+                  daylight = 0;
+            else if (daylight > 15)
+                  daylight = 15;
+
+            buttonState = encoder.getButton();
+            display.setIntensity(daylight);
+            if (millis() - lastMovedScolling >= MOVE_INTERVAL)
+            {
+                  Scollingtext();
+                  lastMovedScolling = millis();
+            }
+      }
+      EEPROM.put(daylightAddr, daylight);
+}
+void Scollingtext()
+{
+      char printtodisp[] = {"Set brightness"};
+      if (doneWithScolling)
+      {
+            doneWithScolling = 0;
+      }
+      else
+      {
+
+            display.sendSmooth(printtodisp, scollingMessageOffset);
+            // next time show one pixel onwards
+            if (scollingMessageOffset++ >= (int)(strlen(printtodisp) * 8))
+            { //if we have run though the display
+                  doneWithScolling = 1;
+                  scollingMessageOffset = -chips * 8;
+            }
       }
 }
